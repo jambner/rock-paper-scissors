addfc49af985cf582617fabbb1fe391055437931
jambner <jambosucks@gmail.com>
2022-07-15 15:55:45 -0500

    Finished Function gameround need to do function game and work on the html and css

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 /* 1. Make the scissors/rock/paper into an array
 2. make the function randomize */
 
-
-function computerPlay (numoftries) {
+function computerPlay () {
     const range = ["rock","paper","scissors"];
     const randomselect = range[Math.floor(Math.random() * range.length)];
-    return randomselect;
+    return randomselect; 
 }
-
-console.log(computerPlay());
-
 function gameround (playerSelection, ComputerSelection) {
     if (playerSelection === ComputerSelection) {
         return 'It\'s a draw !';
       } else if (playerSelection === 'rock') {
-            return ComputerSelection === 'paper' ? 'Computer wins' : 'You win !';
+            return (ComputerSelection === 'paper') ? 'Computer wins' : 'You win !';
       } else if (playerSelection === 'paper') {
-            return ComputerSelection === 'scissors' ? 'Computer wins' : 'You win !';
+            return (ComputerSelection === 'scissors') ? 'Computer wins' : 'You win !';
       } else if (playerSelection === 'scissors') {
-            return ComputerSelection === 'rock' ? 'Computer wins' : 'You win !';
+            return (ComputerSelection === 'rock') ? 'Computer wins' : 'You win !';
       }
 }
-/* ^ Find a way to make the string have value?? ^ */
-const playerSelection = "rock";
-console.log(playerSelection);
+/* ^ Function wont work with computerplay or playerselection?*/
 const ComputerSelection = computerPlay();
+const playerSelection = "rock";
+console.log(ComputerSelection);
 console.log(gameround(playerSelection, ComputerSelection));
 
 function game () {
+    for (let i = 0; i < 5; i++) {
+        
+    }
+
     
 }
