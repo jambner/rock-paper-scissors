f5f6af940878316f1819259e2023537717f245a4
jambner <jambosucks@gmail.com>
2022-07-15 12:23:49 -0500

    Added game round function however, need to find a way to maybe have the strings have value so the conditional works?

--- main.js
+++ main.js
@@ -3,10 +3,30 @@
 
 
 function computerPlay (numoftries) {
-    const range = ["Rock","Paper","Scissors"];
+    const range = ["rock","paper","scissors"];
     const randomselect = range[Math.floor(Math.random() * range.length)];
     return randomselect;
 }
 
 console.log(computerPlay());
 
+function gameround (playerSelection, ComputerSelection) {
+    if (playerSelection === ComputerSelection) {
+        return 'It\'s a draw !';
+      } else if (playerSelection === 'rock') {
+            return ComputerSelection === 'paper' ? 'Computer wins' : 'You win !';
+      } else if (playerSelection === 'paper') {
+            return ComputerSelection === 'scissors' ? 'Computer wins' : 'You win !';
+      } else if (playerSelection === 'scissors') {
+            return ComputerSelection === 'rock' ? 'Computer wins' : 'You win !';
+      }
+}
+/* ^ Find a way to make the string have value?? ^ */
+const playerSelection = "rock";
+console.log(playerSelection);
+const ComputerSelection = computerPlay();
+console.log(gameround(playerSelection, ComputerSelection));
+
+function game () {
+    
+}
