3a0ab87dc9330d5546521338a0b0ed8be962fdce
jambner <jambosucks@gmail.com>
2022-07-17 19:56:46 -0500

    Worked on the function playerplay(); next see ifit will display the playerselect so the gameround will start

--- main.js
+++ main.js
@@ -6,27 +6,47 @@ function computerPlay () {
     const randomselect = range[Math.floor(Math.random() * range.length)];
     return randomselect; 
 }
+
+function playerPlay() {
+       let playerbutton1 = document.getElementById("Rock");
+       let playerbutton2 = document.getElementById("Paper");
+       let playerbutton3 = document.getElementById("Scissors");
+
+           if (playerbutton1) {
+                  playerbutton1.addEventListener("click", function(){
+                        playerSelection==="Rock";
+                  }, false);
+            } else if (playerbutton2) {
+                  playerbutton2.addEventListener("click", function(){
+                        playerSelection==="Paper";
+                  });
+            } else if (playerbutton3) {
+                  playerbutton3.addEventListener("click", function(){
+                        playerSelection==="Scissors";
+                  });
+            }
+}
+
+
 function gameround (playerSelection, ComputerSelection) {
     if (playerSelection === ComputerSelection) {
         return 'It\'s a draw !';
-      } else if (playerSelection === 'rock') {
-            return (ComputerSelection === 'paper') ? 'Computer wins' : 'You win !';
-      } else if (playerSelection === 'paper') {
-            return (ComputerSelection === 'scissors') ? 'Computer wins' : 'You win !';
-      } else if (playerSelection === 'scissors') {
-            return (ComputerSelection === 'rock') ? 'Computer wins' : 'You win !';
+      } else if (playerSelection === 'Rock') {
+            return (ComputerSelection === 'Paper') ? 'Computer wins' : 'You win !';
+      } else if (playerSelection === 'Paper') {
+            return (ComputerSelection === 'Scissors') ? 'Computer wins' : 'You win !';
+      } else if (playerSelection === 'Scissors') {
+            return (ComputerSelection === 'Rock') ? 'Computer wins' : 'You win !';
       }
 }
 /* ^ Function wont work with computerplay or playerselection?*/
+const playerSelection = playerPlay();
 const ComputerSelection = computerPlay();
-const playerSelection = "rock";
 console.log(ComputerSelection);
 console.log(gameround(playerSelection, ComputerSelection));
 
 function game () {
     for (let i = 0; i < 5; i++) {
-        
+      gameround();
     }
-
-    
 }
